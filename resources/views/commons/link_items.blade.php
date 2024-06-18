01e7c57caa7421982bf70c554635e56759a10f3c
akira <ktr131rtk1218g.t@gmail.com>
2024-06-18 01:55:25 +0000

    top page

---
+++ resources/views/commons/link_items.blade.php
@@ -0,0 +1,13 @@
+@if (Auth::check())
+    {{-- ユーザー一覧へのリンク --}}
+    <li><a class="link link-hover" href="#">ユーザー一覧</a></li>
+    {{-- ユーザー詳細へのリンク --}}
+    <li><a class="link link-hover" href="#">{{ Auth::user()->name }}のプロフィール</a></li>
+    <li class="divider lg:hidden"></li>
+    {{-- ログアウトへのリンク --}}
+    <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">ログアウト</a></li>
+@else
+    <li><a class="link link-hover" href="{{ route('register')}}">会員登録</a></li>
+    <li class="divider lg:hidden"></li>
+    <li><a class="link link-hover" href="{{ route('login') }}">ログイン</a></li>
+@endif
